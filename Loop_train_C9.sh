ce87e251f4287aee768f03966b3f8457b54ca0b7
Junkai Yang <yjk122@aonkenp920.inf.ed.ac.uk>
2025-05-24 16:06:51 +0100

    update ignore

--- Loop_train_C9.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-python EEG_low_level_average.py --early_stopping=10 --subject_id sub-08 --channels 'O1,Oz,O2,PO3,POz,PO4,Pz,PO7,PO8' --loss=mse --start_time=0 --end_time=0.5 --project ATM_lowlevel --model EEGConformer --average_eeg "$@"
-python EEG_low_level_average.py --early_stopping=10 --subject_id sub-08 --channels 'O1,Oz,O2,PO3,POz,PO4,Pz,PO7,PO8' --loss=mse --start_time=0 --end_time=0.5 --project ATM_lowlevel  --model EEGConformer  "$@"
